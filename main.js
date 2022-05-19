567db23638caf7dd1f28f41527fcad0e548e4ea9
Muath <muath.khalifa@yahoo.com>
2022-05-19 11:58:13 +0100

    v1

---
+++ main.js
@@ -0,0 +1,44 @@
+const seconds = document.getElementById("seconds");
+const minutes = document.getElementById("minutes");
+const hours = document.getElementById("hours");
+const btnStart = document.getElementById("btn-start");
+const btnPause = document.getElementById("btn-pause");
+
+let secTracker = 0;
+let minTracker = 0;
+let hourTracker = 0;
+
+let intervalId;
+
+btnStart.addEventListener('click', function(){
+    intervalId = setInterval(increment, 1000);
+
+})
+
+btnPause.addEventListener('click',function(){
+    clearInterval(intervalId);
+})
+
+function increment(){
+    secTracker++;
+    seconds.innerHTML = secTracker;
+    incrementMinutes();
+    incrementHours();
+
+}
+
+function incrementMinutes(){
+    minutes.innerHTML = minTracker;
+    if (secTracker == 5) {
+        secTracker = -1;
+        minTracker += 1
+    }
+}
+
+function incrementHours(){
+    hours.innerHTML = hourTracker;
+    if (minTracker == 2) {
+        minTracker = 0
+        hourTracker += 1
+    }
+}
